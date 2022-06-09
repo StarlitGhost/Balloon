addb9a9332a282f8839e39fd98b8bd42aec7e9c0
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2022-06-09 02:11:18 +0100

    add theme settings for countdown timer, make system font settings optional

--- theme.lua
+++ theme.lua
@@ -13,42 +13,63 @@ theme.apply = function(theme_settings)
     options.message.height = theme_settings.message.height
     options.message.offset_x = theme_settings.message.textoffsetx
     options.message.offset_y = theme_settings.message.textoffsety
-    options.message.max_length = theme_settings.message.maxlength
+    options.message.max_length = theme_settings.message.maxlength or 75
     local message_languages = {English=theme_settings.message.fontenglish, Japanese=theme_settings.message.fontjapanese}
     options.message.font = message_languages[windower.ffxi.get_info().language]
     options.message.font_size = theme_settings.message.size
-    options.message.font_alpha = theme_settings.message.alpha
+    options.message.font_color = {}
+    options.message.font_color.alpha = theme_settings.message.dialogue.color.alpha
+    options.message.font_color.red = theme_settings.message.dialogue.color.red
+    options.message.font_color.green = theme_settings.message.dialogue.color.green
+    options.message.font_color.blue = theme_settings.message.dialogue.color.blue
 
-    options.message.font_color_red = theme_settings.message.dialogue.color.red
-    options.message.font_color_green = theme_settings.message.dialogue.color.green
-    options.message.font_color_blue = theme_settings.message.dialogue.color.blue
-    options.message.dialogue_reset = ('%d,%d,%d'):format(options.message.font_color_red, options.message.font_color_green, options.message.font_color_blue)
-    options.message.dialogue_items = theme_settings.message.dialogue.items
-    options.message.dialogue_keyitems = theme_settings.message.dialogue.keyitems
-    options.message.dialogue_gear = theme_settings.message.dialogue.gear
-    options.message.dialogue_roe = theme_settings.message.dialogue.roe
+    options.message.dialogue = {}
+    options.message.dialogue.alpha = theme_settings.message.dialogue.color.alpha
+    options.message.dialogue.red = theme_settings.message.dialogue.color.red
+    options.message.dialogue.green = theme_settings.message.dialogue.color.green
+    options.message.dialogue.blue = theme_settings.message.dialogue.color.blue
+    options.message.dialogue.reset = ('%d,%d,%d'):format(options.message.dialogue.red, options.message.dialogue.green, options.message.dialogue.blue)
+    options.message.dialogue.items = theme_settings.message.dialogue.items
+    options.message.dialogue.keyitems = theme_settings.message.dialogue.keyitems
+    options.message.dialogue.gear = theme_settings.message.dialogue.gear
+    options.message.dialogue.roe = theme_settings.message.dialogue.roe
+    options.message.stroke = {}
+    options.message.dialogue.stroke = {}
     if theme_settings.message.dialogue.stroke then
-        options.message.stroke_width = theme_settings.message.dialogue.stroke.width
-        options.message.stroke_alpha = theme_settings.message.dialogue.stroke.alpha
-        options.message.stroke_red = theme_settings.message.dialogue.stroke.red
-        options.message.stroke_green = theme_settings.message.dialogue.stroke.green
-        options.message.stroke_blue = theme_settings.message.dialogue.stroke.blue
+        options.message.stroke.width = theme_settings.message.dialogue.stroke.width
+        options.message.stroke.alpha = theme_settings.message.dialogue.stroke.alpha
+        options.message.stroke.red = theme_settings.message.dialogue.stroke.red
+        options.message.stroke.green = theme_settings.message.dialogue.stroke.green
+        options.message.stroke.blue = theme_settings.message.dialogue.stroke.blue
+        options.message.dialogue.stroke.width = theme_settings.message.dialogue.stroke.width
+        options.message.dialogue.stroke.alpha = theme_settings.message.dialogue.stroke.alpha
+        options.message.dialogue.stroke.red = theme_settings.message.dialogue.stroke.red
+        options.message.dialogue.stroke.green = theme_settings.message.dialogue.stroke.green
+        options.message.dialogue.stroke.blue = theme_settings.message.dialogue.stroke.blue
     end
 
-    options.message.system_red = theme_settings.message.system.color.red
-    options.message.system_green = theme_settings.message.system.color.green
-    options.message.system_blue = theme_settings.message.system.color.blue
-    options.message.system_reset = ('%d,%d,%d'):format(options.message.system_red, options.message.system_green, options.message.system_blue)
-    options.message.system_items = theme_settings.message.system.items
-    options.message.system_keyitems = theme_settings.message.system.keyitems
-    options.message.system_gear = theme_settings.message.system.gear
-    options.message.system_roe = theme_settings.message.system.roe
-    if theme_settings.message.system.stroke then
-        options.message.system_stroke_width = theme_settings.message.system.stroke.width
-        options.message.system_stroke_alpha = theme_settings.message.system.stroke.alpha
-        options.message.system_stroke_red = theme_settings.message.system.stroke.red
-        options.message.system_stroke_green = theme_settings.message.system.stroke.green
-        options.message.system_stroke_blue = theme_settings.message.system.stroke.blue
+    options.message.system = {}
+    if theme_settings.message.system then
+        options.message.system.alpha = theme_settings.message.system.color.alpha
+        options.message.system.red = theme_settings.message.system.color.red
+        options.message.system.green = theme_settings.message.system.color.green
+        options.message.system.blue = theme_settings.message.system.color.blue
+        options.message.system.reset = ('%d,%d,%d'):format(options.message.system.red, options.message.system.green, options.message.system.blue)
+        options.message.system.items = theme_settings.message.system.items
+        options.message.system.keyitems = theme_settings.message.system.keyitems
+        options.message.system.gear = theme_settings.message.system.gear
+        options.message.system.roe = theme_settings.message.system.roe
+        options.message.system.stroke = {}
+        if theme_settings.message.system.stroke then
+            options.message.system.stroke.width = theme_settings.message.system.stroke.width
+            options.message.system.stroke.alpha = theme_settings.message.system.stroke.alpha
+            options.message.system.stroke.red = theme_settings.message.system.stroke.red
+            options.message.system.stroke.green = theme_settings.message.system.stroke.green
+            options.message.system.stroke.blue = theme_settings.message.system.stroke.blue
+        end
+    else
+        -- use dialogue settings if there are no system settings
+        options.message.system = options.message.dialogue
     end
 
     options.name = {}
@@ -61,22 +82,65 @@ theme.apply = function(theme_settings)
     local name_languages = {English=theme_settings.npcname.fontenglish, Japanese=theme_settings.npcname.fontjapanese}
     options.name.font = name_languages[windower.ffxi.get_info().language]
     options.name.font_size = theme_settings.npcname.size
-    options.name.font_alpha = theme_settings.npcname.color.alpha
-    options.name.font_color_red = theme_settings.npcname.color.red
-    options.name.font_color_green = theme_settings.npcname.color.green
-    options.name.font_color_blue = theme_settings.npcname.color.blue
+    options.name.font_color = {}
+    options.name.font_color.alpha = theme_settings.npcname.color.alpha
+    options.name.font_color.red = theme_settings.npcname.color.red
+    options.name.font_color.green = theme_settings.npcname.color.green
+    options.name.font_color.blue = theme_settings.npcname.color.blue
+    options.name.stroke = {}
     if theme_settings.npcname.stroke then
-        options.name.stroke_width = theme_settings.npcname.stroke.width
-        options.name.stroke_alpha = theme_settings.npcname.stroke.alpha
-        options.name.stroke_red = theme_settings.npcname.stroke.red
-        options.name.stroke_green = theme_settings.npcname.stroke.green
-        options.name.stroke_blue = theme_settings.npcname.stroke.blue
+        options.name.stroke.width = theme_settings.npcname.stroke.width
+        options.name.stroke.alpha = theme_settings.npcname.stroke.alpha
+        options.name.stroke.red = theme_settings.npcname.stroke.red
+        options.name.stroke.green = theme_settings.npcname.stroke.green
+        options.name.stroke.blue = theme_settings.npcname.stroke.blue
     end
 
-    options.prompt_width = theme_settings.prompt.width
-    options.prompt_height = theme_settings.prompt.height
-    options.prompt_offset_x = theme_settings.prompt.offsetx
-    options.prompt_offset_y = theme_settings.prompt.offsety
+    if theme_settings.prompt then
+        options.prompt = {}
+        options.prompt.width = theme_settings.prompt.width
+        options.prompt.height = theme_settings.prompt.height
+        options.prompt.offset_x = theme_settings.prompt.offsetx
+        options.prompt.offset_y = theme_settings.prompt.offsety
+    end
+
+    options.timer = {}
+    if theme_settings.timer then
+        options.timer.offset_x = theme_settings.timer.textoffsetx or theme_settings.prompt.offsetx
+        options.timer.offset_y = theme_settings.timer.textoffsety or theme_settings.prompt.offsety
+        local timer_languages = {English=theme_settings.timer.fontenglish or theme_settings.message.fontenglish,
+                                 Japanese=theme_settings.timer.fontjapanese or theme_settings.message.fontjapanese}
+        options.timer.font = timer_languages[windower.ffxi.get_info().language]
+        options.timer.font_size = theme_settings.timer.size or theme_settings.message.size
+
+        options.timer.font_color = {}
+        if theme_settings.timer.color then
+            options.timer.font_color.alpha = theme_settings.timer.color.alpha
+            options.timer.font_color.red = theme_settings.timer.color.red
+            options.timer.font_color.green = theme_settings.timer.color.green
+            options.timer.font_color.blue = theme_settings.timer.color.blue
+        else
+            options.timer.font_color = options.message.font_color
+        end
+        options.timer.stroke = {}
+        if theme_settings.timer.stroke then
+            options.timer.stroke.width = theme_settings.timer.stroke.width
+            options.timer.stroke.alpha = theme_settings.timer.stroke.alpha
+            options.timer.stroke.red = theme_settings.timer.stroke.red
+            options.timer.stroke.green = theme_settings.timer.stroke.green
+            options.timer.stroke.blue = theme_settings.timer.stroke.blue
+        else
+            options.timer.stroke = options.message.stroke
+        end
+    else
+        -- use prompt position and message font settings, if no timer settings exist in the theme
+        options.timer.offset_x = theme_settings.prompt.offsetx
+        options.timer.offset_y = theme_settings.prompt.offsety
+        options.timer.font = options.message.font
+        options.timer.font_size = options.message.font_size
+        options.timer.font_color = options.message.font_color
+        options.timer.stroke = options.message.stroke
+    end
 
     return options
 end
--- ui.lua
+++ ui.lua
@@ -36,11 +36,11 @@ local function setup_text(text, text_options)
     text:bg_visible(false)
     text:font(text_options.font, 'meiryo', 'segoe ui', 'sans-serif')
     text:size(text_options.font_size)
-    text:alpha(text_options.font_alpha)
-    text:color(text_options.font_color_red, text_options.font_color_green, text_options.font_color_blue)
-    text:stroke_transparency(text_options.stroke_alpha or 0)
-    text:stroke_color(text_options.stroke_red or 0, text_options.stroke_green or 0, text_options.stroke_blue or 0)
-    text:stroke_width(text_options.stroke_width or 0)
+    text:alpha(text_options.font_color.alpha)
+    text:color(text_options.font_color.red, text_options.font_color.green, text_options.font_color.blue)
+    text:stroke_transparency(text_options.stroke.alpha or 0)
+    text:stroke_color(text_options.stroke.red or 0, text_options.stroke.green or 0, text_options.stroke.blue or 0)
+    text:stroke_width(text_options.stroke.width or 0)
 end
 
 function ui:load(settings, theme_options)
@@ -49,37 +49,39 @@ function ui:load(settings, theme_options)
 
     self._dialogue_settings.path = theme_options.balloon_background
     self._dialogue_settings.color = {}
-    self._dialogue_settings.color.red = theme_options.message.font_color_red
-    self._dialogue_settings.color.green = theme_options.message.font_color_green
-    self._dialogue_settings.color.blue = theme_options.message.font_color_blue
-    self._dialogue_settings.reset = theme_options.message.dialogue_reset
-    self._dialogue_settings.items = theme_options.message.dialogue_items
-    self._dialogue_settings.keyitems = theme_options.message.dialogue_keyitems
-    self._dialogue_settings.gear = theme_options.message.dialogue_gear
-    self._dialogue_settings.roe = theme_options.message.dialogue_roe
+    self._dialogue_settings.color.alpha = theme_options.message.dialogue.alpha
+    self._dialogue_settings.color.red = theme_options.message.dialogue.red
+    self._dialogue_settings.color.green = theme_options.message.dialogue.green
+    self._dialogue_settings.color.blue = theme_options.message.dialogue.blue
+    self._dialogue_settings.reset = theme_options.message.dialogue.reset
+    self._dialogue_settings.items = theme_options.message.dialogue.items
+    self._dialogue_settings.keyitems = theme_options.message.dialogue.keyitems
+    self._dialogue_settings.gear = theme_options.message.dialogue.gear
+    self._dialogue_settings.roe = theme_options.message.dialogue.roe
     self._dialogue_settings.stroke = {}
-    self._dialogue_settings.stroke.width = theme_options.message.stroke_width or 0
-    self._dialogue_settings.stroke.alpha = theme_options.message.stroke_alpha or 0
-    self._dialogue_settings.stroke.red = theme_options.message.stroke_red or 0
-    self._dialogue_settings.stroke.green = theme_options.message.stroke_green or 0
-    self._dialogue_settings.stroke.blue = theme_options.message.stroke_blue or 0
+    self._dialogue_settings.stroke.width = theme_options.message.dialogue.stroke.width
+    self._dialogue_settings.stroke.alpha = theme_options.message.dialogue.stroke.alpha
+    self._dialogue_settings.stroke.red = theme_options.message.dialogue.stroke.red
+    self._dialogue_settings.stroke.green = theme_options.message.dialogue.stroke.green
+    self._dialogue_settings.stroke.blue = theme_options.message.dialogue.stroke.blue
 
     self._system_settings.path = theme_options.system_background
     self._system_settings.color = {}
-    self._system_settings.color.red = theme_options.message.system_red
-    self._system_settings.color.green = theme_options.message.system_green
-    self._system_settings.color.blue = theme_options.message.system_blue
-    self._system_settings.reset = theme_options.message.system_reset
-    self._system_settings.items = theme_options.message.system_items
-    self._system_settings.keyitems = theme_options.message.system_keyitems
-    self._system_settings.gear = theme_options.message.system_gear
-    self._system_settings.roe = theme_options.message.system_roe
+    self._system_settings.color.alpha = theme_options.message.system.alpha
+    self._system_settings.color.red = theme_options.message.system.red
+    self._system_settings.color.green = theme_options.message.system.green
+    self._system_settings.color.blue = theme_options.message.system.blue
+    self._system_settings.reset = theme_options.message.system.reset
+    self._system_settings.items = theme_options.message.system.items
+    self._system_settings.keyitems = theme_options.message.system.keyitems
+    self._system_settings.gear = theme_options.message.system.gear
+    self._system_settings.roe = theme_options.message.system.roe
     self._system_settings.stroke = {}
-    self._system_settings.stroke.width = theme_options.message.system_stroke_width or 0
-    self._system_settings.stroke.alpha = theme_options.message.system_stroke_alpha or 0
-    self._system_settings.stroke.red = theme_options.message.system_stroke_red or 0
-    self._system_settings.stroke.green = theme_options.message.system_stroke_green or 0
-    self._system_settings.stroke.blue = theme_options.message.system_stroke_blue or 0
+    self._system_settings.stroke.width = theme_options.message.system.stroke.width
+    self._system_settings.stroke.alpha = theme_options.message.system.stroke.alpha
+    self._system_settings.stroke.red = theme_options.message.system.stroke.red
+    self._system_settings.stroke.green = theme_options.message.system.stroke.green
+    self._system_settings.stroke.blue = theme_options.message.system.stroke.blue
 
     self._type = self._dialogue_settings
 
@@ -89,7 +91,7 @@ function ui:load(settings, theme_options)
 
     setup_text(self.message_text, theme_options.message)
     setup_text(self.name_text, theme_options.name)
-    setup_text(self.timer_text, theme_options.message)
+    setup_text(self.timer_text, theme_options.timer)
 
     self:position(settings, theme_options)
 
@@ -105,12 +107,14 @@ function ui:position(settings, theme_options)
     local y = settings.Position.Y - center_offset_y * settings.Scale
     local name_bg_offset_x = theme_options.name.background_offset_x * settings.Scale
     local name_bg_offset_y = theme_options.name.background_offset_y * settings.Scale
-    local prompt_offset_x = theme_options.prompt_offset_x * settings.Scale
-    local prompt_offset_y = theme_options.prompt_offset_y * settings.Scale
+    local prompt_offset_x = theme_options.prompt.offset_x * settings.Scale
+    local prompt_offset_y = theme_options.prompt.offset_y * settings.Scale
     local message_text_offset_x = theme_options.message.offset_x * settings.Scale
     local message_text_offset_y = theme_options.message.offset_y * settings.Scale
     local name_text_offset_x = theme_options.name.offset_x * settings.Scale
     local name_text_offset_y = theme_options.name.offset_y * settings.Scale
+    local timer_text_offset_x = theme_options.timer.offset_x * settings.Scale
+    local timer_text_offset_y = theme_options.timer.offset_y * settings.Scale
 
     self.message_background:pos(x, y)
     self.name_background:pos(x + name_bg_offset_x, y + name_bg_offset_y)
@@ -118,15 +122,15 @@ function ui:position(settings, theme_options)
 
     self.message_text:pos(x + message_text_offset_x, y + message_text_offset_y)
     self.name_text:pos(x + name_text_offset_x, y + name_text_offset_y)
-    self.timer_text:pos(x + prompt_offset_x, y + prompt_offset_y)
+    self.timer_text:pos(x + timer_text_offset_x, y + timer_text_offset_y)
 
     self.message_background:size(theme_options.message.width * settings.Scale, theme_options.message.height * settings.Scale)
     self.name_background:size(theme_options.name.width * settings.Scale, theme_options.name.height * settings.Scale)
-    self.prompt:size(theme_options.prompt_width * settings.Scale, theme_options.prompt_height * settings.Scale)
+    self.prompt:size(theme_options.prompt.width * settings.Scale, theme_options.prompt.height * settings.Scale)
 
     self.message_text:size(theme_options.message.font_size * settings.Scale)
     self.name_text:size(theme_options.name.font_size * settings.Scale)
-    self.timer_text:size(theme_options.message.font_size * settings.Scale)
+    self.timer_text:size(theme_options.timer.font_size * settings.Scale)
 end
 
 function ui:hide()
@@ -173,15 +177,11 @@ function ui:set_type(type)
     self._type = types[type]
 
     self:update_message_bg(self._type.path)
+    self.message_text:alpha(self._type.color.alpha)
     self.message_text:color(self._type.color.red, self._type.color.green, self._type.color.blue)
     self.message_text:stroke_transparency(self._type.stroke.alpha)
     self.message_text:stroke_color(self._type.stroke.red, self._type.stroke.green, self._type.stroke.blue)
     self.message_text:stroke_width(self._type.stroke.width)
-
-    self.timer_text:color(self._type.color.red, self._type.color.green, self._type.color.blue)
-    self.timer_text:stroke_transparency(self._type.stroke.alpha)
-    self.timer_text:stroke_color(self._type.stroke.red, self._type.stroke.green, self._type.stroke.blue)
-    self.timer_text:stroke_width(self._type.stroke.width)
 end
 
 function ui:set_character(name)
@@ -215,7 +215,7 @@ function ui:animate_prompt(frame_count, theme_options)
     local amplitude = 2.5
 	local bounceOffset = smooth_sawtooth(frame_count/60, 6) * amplitude
 
-	local pos_y = self.message_background:pos_y() + (theme_options.prompt_offset_y + bounceOffset) * self._scale
+	local pos_y = self.message_background:pos_y() + (theme_options.prompt.offset_y + bounceOffset) * self._scale
 	self.prompt:pos_y(pos_y)
 end
 
